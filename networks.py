f3f6dd4ceece6ab8027cae3b8118204bd5b19d48
andreasoie <oeie.andreas@gmail.com>
2023-05-12 21:55:27 +0200

    Update ReLU

--- networks.py
+++ networks.py
@@ -1015,8 +1015,13 @@ class ResnetGenerator(nn.Module):
             use_bias = norm_layer.func == nn.InstanceNorm2d
         else:
             use_bias = norm_layer == nn.InstanceNorm2d
-
-        model = [nn.ReflectionPad2d(3), nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias), norm_layer(ngf), nn.ReLU(True)]
+        
+        model = [
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+            norm_layer(ngf),
+            nn.ReLU(inplace=True),
+        ]
 
         n_downsampling = 2
         for i in range(n_downsampling):  # add downsampling layers
@@ -1025,13 +1030,13 @@ class ResnetGenerator(nn.Module):
                 model += [
                     nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
                     norm_layer(ngf * mult * 2),
-                    nn.ReLU(True),
+                    nn.ReLU(inplace=True),
                 ]
             else:
                 model += [
                     nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=1, padding=1, bias=use_bias),
                     norm_layer(ngf * mult * 2),
-                    nn.ReLU(True),
+                    nn.ReLU(inplace=True),
                     Downsample(ngf * mult * 2),
                 ]
 
@@ -1045,14 +1050,14 @@ class ResnetGenerator(nn.Module):
                 model += [
                     nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
                     norm_layer(int(ngf * mult / 2)),
-                    nn.ReLU(True),
+                    nn.ReLU(inplace=True),
                 ]
             else:
                 model += [
                     Upsample(ngf * mult),
                     nn.Conv2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=1, padding=1, bias=use_bias),  # output_padding=1,
                     norm_layer(int(ngf * mult / 2)),
-                    nn.ReLU(True),
+                    nn.ReLU(inplace=True),
                 ]
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
